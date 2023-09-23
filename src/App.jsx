0b8927ad30d20824684fd201a8718606e6274c4c
Ambrish <ambrish19.5211@gmail.com>
2023-09-24 04:01:06 +0530

    pokemon details component refined

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import Pokedex from './Components/Pokedex/Pokedex'
 import './App.css'
 import Search from './Components/Search/Search'
+import { Router, Route, Routes } from 'react-router-dom'
+import PokemonDetails from './Components/PokemonDetails/PokemonDetails'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Pokedex/>
-    </>
+    <Routes>
+      <Route path="/" element={<Pokedex/>}></Route>
+      <Route path="/pokemon/:id" element={<PokemonDetails/>}></Route>
+      <Route path="*" element={<h1>Error 404! Path not found</h1> }></Route>
+    </Routes>
   )
 }
 
--- src/Components/Pokemon/Pokemon.jsx
+++ src/Components/Pokemon/Pokemon.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import './Pokemon.css'
-
-const Pokemon = ({name, url, id}) => {
-  return (
-    <div className='pokemon'>
+// CSS imports
+import './Pokemon.css';
+import { Link } from 'react-router-dom';
+function Pokemon({ name, url, id }) {
+    return (
+        <Link to={`/pokemon/${id}`} className='pokemon-wrapper'> 
+            <div className='pokemon'>
                     <div className='pokemon-name'>{name}</div>
                     <img className='pokemon-image' src={url} />
             </div>
-  )
+        </Link>
+
+    )
 }
 
-export default Pokemon
+export default Pokemon;
